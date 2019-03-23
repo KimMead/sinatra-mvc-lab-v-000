268ec83c67a9777c9faadff9dbe1988438a85189
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-03-23 01:00:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,17 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-      erb :user_input
-    end
+    erb :user_input
+  end
 
-    post '/piglatinize' do
-      lang = PigLatinizer.new
-      @piglatinized = lang.to_pig_latin(params[:user_phrase])
-    end
-end
-
-  post '/piglatinize again' do
-    @results = PigLatinizer.new(params[:user_phrase])
-    erb :results
+  post '/piglatinize' do
+    lang = PigLatinizer.new
+    @piglatinized = lang.to_pig_latin(params[:user_phrase])
   end
 end
+
+#   post '/piglatinize again' do
+#     @results = PigLatinizer.new(params[:user_phrase])
+#     erb :results
+#   end
+# end
